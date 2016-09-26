07429674cbe7d6009d7c6f02e6c2d8f41f01e9d2
jahanpd <jahan.pd@gmail.com>
2016-09-27 07:01:34 +1000

    gradient descent

--- ML.py
+++ ML.py
@@ -1,6 +1,7 @@
 import data as d
 import initialTheta
 import numpy as np
+from sympy import *
 import costFunction as cf
 import os
 
@@ -11,8 +12,10 @@ csv = os.path.realpath(
 X_trn, X_tst, y_trn, y_tst, m_trn, n_trn, m_tst, n_tst = d.import_data(csv)
 
 y_trn = d.transform_y(y_trn)
+y_trn = Matrix(y_trn)
 
 X_trn = d.normalise(X_trn)
+X_trn = Matrix(X_trn)
 
 # hidden layers
 hl = 5
@@ -26,13 +29,14 @@ Th1 = initialTheta.Theta1(n_trn, nl, out)
 
 hl_thetas = initialTheta.ThetaHL(n_trn, nl, out, hl)
 Th2, Th3, Th4, Th5 = np.array_split(hl_thetas, 4)
-Th2 = np.reshape(Th2, (nl+1, nl))
-Th2 = np.reshape(Th2, (nl+1, nl))
-Th3 = np.reshape(Th3, (nl+1, nl))
-Th3 = np.reshape(Th3, (nl+1, nl))
-Th4 = np.reshape(Th4, (nl+1, nl))
-Th5 = np.reshape(Th5, (nl+1, nl))
+Th1 = Matrix(Th1)
+Th2 = Matrix(np.reshape(Th2, (nl+1, nl)))
+Th3 = Matrix(np.reshape(Th3, (nl+1, nl)))
+Th4 = Matrix(np.reshape(Th4, (nl+1, nl)))
+Th5 = Matrix(np.reshape(Th5, (nl+1, nl)))
 
 ThOut = initialTheta.ThetaOut(n_trn, nl, out)
+ThOut = Matrix(ThOut)
 
-cf.costFunction(X_trn, y_trn, Th1, Th2, Th3, Th4, Th5, ThOut, m_trn, n_trn)
+a2, a3, a4, a5, a6, h0 = cf.costFunction(X_trn, y_trn, Th1, Th2, Th3, Th4, Th5, ThOut, m_trn, n_trn)
+cf.gradient_descent(a2, a3, a4, a5, a6, h0, X_trn, y_trn, Th1, Th2, Th3, Th4, Th5, ThOut, m_trn, n_trn)
--- costFunction.py
+++ costFunction.py
@@ -1,6 +1,5 @@
 import numpy as np
 from sympy import *
-from sigmoid import sigmoid
 import mpmath as mp
 
 # sympy elementwise functions
@@ -12,57 +11,38 @@ sqar = lambda x: mp.power(x, 2)
 
 
 def costFunction(X_trn, y_trn, Th1, Th2, Th3, Th4, Th5, ThOut, m_trn, n_trn):
-    X_trn = Matrix(X_trn)
-    print(X_trn.shape)
     X_trn = X_trn.col_insert(0, ones(m_trn, 1))
-    Th1 = Matrix(Th1)
     a2 = X_trn*Th1
     a2 = a2.applyfunc(sig)
-    print(a2.shape)
 
     a2 = a2.col_insert(0, ones(m_trn, 1))
-    Th2 = Matrix(Th2)
     a3 = a2*Th2
     a3 = a3.applyfunc(sig)
-    print(a3.shape)
 
     a3 = a3.col_insert(0, ones(m_trn, 1))
-    Th3 = Matrix(Th3)
     a4 = a3*Th3
     a4 = a4.applyfunc(sig)
-    print(a4.shape)
 
     a4 = a4.col_insert(0, ones(m_trn, 1))
-    Th4 = Matrix(Th4)
     a5 = a4*Th4
     a5 = a5.applyfunc(sig)
-    print(a5.shape)
 
     a5 = a5.col_insert(0, ones(m_trn, 1))
-    Th5 = Matrix(Th5)
     a6 = a5*Th5
     a6 = a6.applyfunc(sig)
-    print(a6.shape)
 
     a6 = a6.col_insert(0, ones(m_trn, 1))
-    ThOut = Matrix(ThOut)
     h0 = a6*ThOut
     h0 = h0.applyfunc(sig)
-    print(h0.shape)
 
-    y_trn = Matrix(y_trn)
-    print(y_trn.shape)
     j1h0 = h0.applyfunc(mlog)
     j1h0 = j1h0.transpose()
-    print(j1h0.shape)
     j2h0 = h0.applyfunc(sub1)
     j2h0 = j2h0.applyfunc(mlog)
     j2h0 = j2h0.transpose()
     j2y = y_trn.applyfunc(sub1)
     Jpart1 = y_trn*j1h0
-    print(Jpart1.shape)
     Jpart2 = j2y*j2h0
-    print(Jpart2.shape)
     J = Jpart1 + Jpart2
     J = np.array(J).astype(np.float64)
     J = np.sum(J)
@@ -91,3 +71,14 @@ def costFunction(X_trn, y_trn, Th1, Th2, Th3, Th4, Th5, ThOut, m_trn, n_trn):
     ThetaReg = (np.sum(Th1r) + np.sum(Th2r) + np.sum(Th3r) + np.sum(Th4r) + np.sum(Th5r) + np.sum(ThOutr))
     J = J + ThetaReg
     print(J)
+    return a2, a3, a4, a5, a6, h0
+
+def gradient_descent(a2, a3, a4, a5, a6, h0, X_trn, y_trn, Th1, Th2, Th3, Th4, Th5, ThOut, m_trn, n_trn):
+    d7 = h0 - y_trn
+    d6 = (ThOut)*(d7.T)
+    d6 = d6.T
+    print(d6.shape)
+    a61 = a6.applyfunc(sub1)
+    print(a6.shape)
+    print(a61.shape)
+    a6dif = (a6*a61)
